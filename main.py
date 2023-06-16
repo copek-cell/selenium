8060c3caabd348b0dcb67c437a9a685071d73137
copek-cell <112305760+copek-cell@users.noreply.github.com>
2023-06-16 21:19:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from bs4 import BeautifulSoup
 from pydantic import BaseModel
@@ -7,14 +7,16 @@ import httpx
 import time
 import concurrent.futures
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from enum import Enum
+import psutil
+from typing import List, Dict, Optional
+import cpuinfo
 
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000",
     "https://kikisan.pages.dev",
     "https://kikisan.site",
     "https://www.kikisan.site",
@@ -29,23 +31,75 @@ app.add_middleware(
 )
 
 
-class DataRequest(BaseModel):
-    pages: int
+class Status(str, Enum):
+    def __str__(self):
+        return str(self.value)
+
+    BARU = "baru"
+    LAMA = "lama"
+
+
+class Hasil(BaseModel):
     keyword: str
-    userAgent: str
+    pages: int
+    status: Status
+    upload: str
+    download: str
+    time: float
+    cpu_type: Optional[str] = None
+    cpu_threads_max: Optional[int] = None
+    cpu_frequency_max: Optional[float] = None
+    cpu_max_percent: Optional[List] = None
+    ram_total: Optional[str] = None
+    ram_min_available: Optional[str] = None
+    ram_max_percent: Optional[float] = None
+    jumlah: int
+    hasil: Optional[List[Dict]] = None
+
+
+class DataRequest(BaseModel):
+    keyword: str
+    pages: int
+
+
+data_seleniumhttpx = []
 
 
 @app.post("/seleniumhttpx")
-def index(data: DataRequest):
+def input_seleniumhttpx(request: Request, input: DataRequest):
     try:
-        headers = {"User-Agent": data.userAgent}
+        sent_bytes_start, received_bytes_start = get_network_usage()
+
+        headers = {"User-Agent": request.headers.get("User-Agent")}
+
         start_time = time.time()
-        loop = main(headers, data.keyword, data.pages)
+        hasil = main(headers, input.keyword, input.pages)
         end_time = time.time()
+
+        sent_bytes_end, received_bytes_end = get_network_usage()
+
+        sent_bytes_total = sent_bytes_end - sent_bytes_start
+        received_bytes_total = received_bytes_end - received_bytes_start
+
+        print("Total Penggunaan Internet:")
+        print("Upload:", format_bytes(sent_bytes_total))
+        print("Download:", format_bytes(received_bytes_total))
+
         print(
-            f"Berhasil mengambil {len(loop)} produk dalam {end_time - start_time} detik."
+            f"Berhasil mengambil {len(hasil)} produk dalam {end_time - start_time} detik."
         )
-        return loop
+        data = {
+            "keyword": input.keyword,
+            "pages": input.pages,
+            "status": "baru",
+            "upload": format_bytes(sent_bytes_total),
+            "download": format_bytes(received_bytes_total),
+            "time": end_time - start_time,
+            "jumlah": len(hasil),
+            "hasil": hasil,
+        }
+        data_seleniumhttpx.append(data)
+        return data_seleniumhttpx
     except Exception as e:
         return e
 
@@ -87,18 +141,15 @@ def main(headers, keyword, pages):
 
 def scrape(keyword, page):
     soup_produk = []
-    opsi = webdriver.ChromeOptions()
+    # chrome_options = Options()
+    # chrome_options.add_argument("--headless")
 
-    opsi.add_experimental_option("excludeSwitches", ["enable-logging"])
-    opsi.add_argument("--headless")
-    servis = Service("./chromedriver")
+    driver = webdriver.Chrome("./chromedriver")
     try:
         # await asyncio.sleep(2)
-        driver = webdriver.Chrome(service=servis, options=opsi)
-        driver.set_page_load_timeout(30)
-        driver.maximize_window()
+        print(f"Membuka page {page}...")
         driver.get(f"https://www.tokopedia.com/search?q={keyword}&page={page}")
-        print("Menunggu reload...")
+        print(f"Menunggu reload page {page}...")
         time.sleep(
             5
         )  # Tunggu beberapa detik untuk memastikan halaman telah selesai dimuat
@@ -133,21 +184,6 @@ def scrape(keyword, page):
         return soup_produk
     except Exception as e:
         print(f"Terjadi kesalahan saat mengakses halaman {page}: {str(e)}")
-
-
-def scroll(page, scroll_amount):
-    try:
-        prev_height = page.evaluate("document.documentElement.scrollTop")
-        while True:
-            page.wait_for_selector(".css-974ipl", timeout=1800000)
-            page.evaluate(f"window.scrollBy(0, {scroll_amount});")
-            curr_height = page.evaluate("document.documentElement.scrollTop")
-            if prev_height == curr_height:
-                break
-            prev_height = curr_height
-            print("Scrolling...")
-    except Exception as e:
-        print(f"Terjadi kesalahan saat Scrolling: {str(e)}")
 
 
 async def scrape_produk(product_soup, headers, session):
@@ -354,3 +390,108 @@ def data_shop(shop_link, session, headers):
     else:
         print(f"Gagal melakukan koneksi ke {shop_link} setelah mencoba beberapa kali.")
         return {}
+
+
+def get_network_usage():
+    network_stats = psutil.net_io_counters()
+    sent_bytes = network_stats.bytes_sent
+    received_bytes = network_stats.bytes_recv
+
+    return sent_bytes, received_bytes
+
+
+def format_bytes(bytes):
+    # Fungsi ini mengubah ukuran byte menjadi format yang lebih mudah dibaca
+    sizes = ["B", "KB", "MB", "GB", "TB"]
+    i = 0
+    while bytes >= 1024 and i < len(sizes) - 1:
+        bytes /= 1024
+        i += 1
+    return "{:.2f} {}".format(bytes, sizes[i])
+
+
+@app.get("/data")
+def ambil_data(
+    keyword: Optional[str] = None,
+    pages: Optional[int] = None,
+    status: Optional[Status] = None,
+):
+    if status is not None or keyword is not None or pages is not None:
+        result_filter = []
+        for data in data_seleniumhttpx:
+            data = Hasil.parse_obj(data)
+            if (
+                status == data.status
+                and data.keyword == keyword
+                and data.pages == pages
+            ):
+                result_filter.append(data)
+    else:
+        result_filter = data_seleniumhttpx
+    return result_filter
+
+
+@app.put("/monitoring")
+def ambil_data(input: DataRequest):
+    for data in data_seleniumhttpx:
+        if (
+            data["status"] == "baru"
+            and data["keyword"] == input.keyword
+            and data["pages"] == input.pages
+        ):
+            cpu_info = cpuinfo.get_cpu_info()
+            cpu_type = cpu_info["brand_raw"]
+            print("Tipe CPU:", cpu_type)
+
+            ram = psutil.virtual_memory()
+            ram_total = ram.total  # Total RAM dalam bytes
+            print("Total RAM:", ram_total)
+
+            cpu_percent_max = []  # Highest CPU usage during execution
+            cpu_frequency_max = 0  # Highest frekuensi CPU usage during execution
+            cpu_threads_max = 0  # Highest thread CPU usage during execution
+            ram_percent_max = 0  # Highest RAM usage during execution
+            ram_available_min = 0  # Highest RAM usage during execution
+
+            interval = 0.1  # Interval for monitoring (seconds)
+            duration = data["time"]
+            num_intervals = int(duration / interval) + 1
+
+            for _ in range(num_intervals):
+                cpu_threads = psutil.cpu_count(logical=True)
+                print("thread cpu", cpu_threads)
+                cpu_frequency = psutil.cpu_freq().current
+                print("frekuensi cpu", cpu_frequency)
+                cpu_percent = psutil.cpu_percent(interval=interval, percpu=True)
+                print("cpu", cpu_percent)
+                # Informasi RAM
+                ram_percent = psutil.virtual_memory().percent
+                print("ram", ram_percent)
+                ram_available = ram.available  # RAM yang tersedia dalam bytes
+                print("RAM Tersedia:", ram_available)
+
+                if cpu_threads > cpu_threads_max:
+                    cpu_threads_max = cpu_threads
+
+                if cpu_frequency > cpu_frequency_max:
+                    cpu_frequency_max = cpu_frequency
+
+                if cpu_percent > cpu_percent_max:
+                    cpu_percent_max = cpu_percent
+
+                if ram_percent > ram_percent_max:
+                    ram_percent_max = ram_percent
+
+                if ram_available < ram_available_min:
+                    ram_available_min = ram_available
+
+            data["cpu_type"] = cpu_type
+            data["cpu_threads_max"] = cpu_threads_max
+            data["cpu_frequency_max"] = cpu_frequency_max
+            data["cpu_max_percent"] = cpu_percent_max
+            data["ram_total"] = format_bytes(ram_total)
+            data["ram_max_percent"] = ram_percent_max
+            data["ram_min_available"] = format_bytes(ram_available_min)
+            data["status"] = "lama"
+
+    return data_seleniumhttpx
